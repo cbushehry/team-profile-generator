3af55db470227771f62804d401651e1d3b4591d6
cbushehry <c.bushehry@gmail.com>
2024-07-25 22:26:40 -0700

    Set up initial prompts for manager details in index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Import necessary modules
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
@@ -5,3 +6,60 @@ const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
+
+// Array to hold team members
+const team = [];
+
+// Function to prompt for manager details
+const promptManager = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the team manager’s name?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the team manager’s ID?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the team manager’s email?',
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'What is the team manager’s office number?',
+    }
+  ]).then(managerData => {
+    const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber);
+    team.push(manager);
+    promptMenu(); // Call the menu to add more members or finish
+  });
+};
+
+// Function to present the menu to the user
+const promptMenu = () => {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'menu',
+      message: 'Would you like to add an engineer, an intern, or finish building your team?',
+      choices: ['Add an engineer', 'Add an intern', 'Finish building my team']
+    }
+  ]).then(userChoice => {
+    switch (userChoice.menu) {
+      case 'Add an engineer':
+        promptEngineer();
+        break;
+      case 'Add an intern':
+        promptIntern();
+        break;
+      default:
+        buildTeam();
+    }
+  });
+};
+
